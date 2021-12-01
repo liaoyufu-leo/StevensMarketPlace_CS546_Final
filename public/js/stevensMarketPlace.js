44fd03ed2aea8eb4e7b1017a982dcc870f807822
Venkatsaik <Venkatsaik101@gmail.com>
2021-12-01 18:11:59 -0500

    cart handlebars and routes related to cart were modified

--- public/js/stevensMarketPlace.js
+++ public/js/stevensMarketPlace.js
@@ -45,11 +45,12 @@ function search() {
                 <div class="row">
                   <h5 class="col-6 text-danger fw-bold">$${element.price}</h5>
                   <p class="col-6 text-end text-dark">${new Date(element.date).toISOString().split('T')[0]}</p>
-                  <a href='cart' class="btn btn-danger" id="likeID">Add to cart</a>
-                  <a href='Buy' class="btn btn-danger" id="likeID">Buy</a>
-
-
+                  <a href="/user/addCart/${element._id}" class="btn btn-danger" id="likeID">Add to cart</a>
+                  
+                  
                 </div>
+
+                
                 
               </div>
             </div>
